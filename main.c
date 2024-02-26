dd1f68d59befb556cd9f81ecb2ed96e188d3f8de
Raskovalov <149192370+Raskovalov@users.noreply.github.com>
2024-02-26 17:36:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 
 #define sizeB(a) (sizeof(a)/sizeof 0[a]) 
 
-cvector_vector_type(cvector_vector_type(char)) *pathGraph = NULL;
-cvector_vector_type(cvector_vector_type(char)) *graph = NULL;
+cvector_vector_type(cvector_vector_type(char))* numberEdges = NULL;
+cvector_vector_type(cvector_vector_type(char))* pathGraph = NULL;
+cvector_vector_type(cvector_vector_type(char))* graph = NULL;
 cvector_vector_type(char*) useEdges = NULL;
+int maxEdges = 0;
 
 void ReadGraph() {
 	FILE* f = fopen("graph.txt", "r");
@@ -73,50 +75,65 @@ void StartSerchGraph() {
 			cvector_push_back(line, graph[i][1]);
 
 			cvector_push_back(pathGraph, line);
-			cvector_push_back(useEdges, graph[i][0]);
+			cvector_push_back(useEdges, graph[i][1]);
 		}
 	}
 }
 
 int SerchGraph() {
 	cvector_vector_type(cvector_vector_type(char)) *resPathGraph = NULL;
-	cvector_vector_type(char) *resUseEdges = useEdges;
+	cvector_vector_type(char) *resUseEdges = NULL;
 	int eventsUseEdges = 0;
+	int events = 1;
+	char* c = NULL;
+	char* c1 = NULL;
 
 	for (int i = 0; i < cvector_size(graph);i++) {
-		for (int p = 0; p < cvector_size(pathGraph); p++) {
-			char* c = pathGraph[p][cvector_size(pathGraph[p]) - 1];
-			char* c1 = graph[i][0];
-			if (!strcmp(c, c1)) { //Проверка на совместимость вершин
-				eventsUseEdges = 0;
+		events = 1;
+		for (int j = 0; j < cvector_size(useEdges); j++) {//Проверка на исп вер
+			c = graph[i][0];
+			c1 = useEdges[j];
+			if (!strcmp(c, c1)) {
+				events = 0;
+				break;
+			}
+		}
 
-				for (int j = 0; j < cvector_size(pathGraph[p]);j++) {//Проверка на повторы в пути
-					c = pathGraph[p][j];
-					c1 = graph[i][1];
-					if (!strcmp(c, c1)) {
-						eventsUseEdges = 1;
-					}
-				}
+		if (events == 0) {
+			for (int p = 0; p < cvector_size(pathGraph); p++) {
+				c = pathGraph[p][cvector_size(pathGraph[p]) - 1];
+				c1 = graph[i][0];
+				if (!strcmp(c, c1)) { //Проверка на совместимость вершин
+					eventsUseEdges = 0;
 
-				if (eventsUseEdges == 0) {
-					cvector_vector_type(char)* line = NULL;
-					for (int j = 0; j < cvector_size(pathGraph[p]);j++) {//Добавляем путь в список
-						cvector_push_back(line, pathGraph[p][j]);
-						//printf("%s -> ", pathGraph[p][j]);
-					}
-					//printf("%s\n", graph[i][1]);
-					cvector_push_back(line, graph[i][1]);
-					cvector_push_back(resPathGraph, line);
-					cvector_push_back(resUseEdges, graph[i][1]);
-
-					c = graph[i][1];
-					c1 = graph[cvector_size(graph) - 1][0];
-					if (!strcmp(c, c1)) {
-						for (int j = 0; j < cvector_size(line);j++) {
-							printf("%s -> ", line[j]);
+					for (int j = 0; j < cvector_size(pathGraph[p]);j++) {//Проверка на повторы в пути
+						c = pathGraph[p][j];
+						c1 = graph[i][1];
+						if (!strcmp(c, c1)) {
+							eventsUseEdges = 1;
 						}
+					}
 
-						return 1;
+					if (eventsUseEdges == 0) {
+						cvector_vector_type(char)* line = NULL;
+						for (int j = 0; j < cvector_size(pathGraph[p]);j++) {//Добавляем путь в список
+							cvector_push_back(line, pathGraph[p][j]);
+							//printf("%s -> ", pathGraph[p][j]);
+						}
+						//printf("%s\n", graph[i][1]);
+						cvector_push_back(line, graph[i][1]);
+						cvector_push_back(resPathGraph, line);
+						cvector_push_back(resUseEdges, graph[i][1]);
+
+						c = graph[i][1];
+						c1 = graph[cvector_size(graph) - 1][0];
+						if (!strcmp(c, c1)) {
+							for (int j = 0; j < cvector_size(line);j++) {
+								printf("%s -> ", line[j]);
+							}
+
+							return 1;
+						}
 					}
 				}
 			}
